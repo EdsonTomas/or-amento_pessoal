9bc8d3c3f83e64ae1d516db3407a787500a247d3
Edson Tomas <tomasedson1@hotmail.com>
2021-04-19 15:36:18 +0100

    recebendo dados do front

---
+++ arquivos_iniciais/app.js
@@ -0,0 +1,7 @@
+function salvar(){
+    let ano = document.getElementById('ano');
+    let mes = document.getElementById('mes');
+    let dia = document.getElementById('dia');
+    let tipo = document.getElementById('tipo');
+    console.log(ano.value, mes.value, dia.value,tipo.value)
+}

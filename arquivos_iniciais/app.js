b3dbce7feae26c309c9fc091895c06befe31c761
Edson Tomas <tomasedson1@hotmail.com>
2021-04-23 23:47:02 +0100

    listar despesas parte 1

--- arquivos_iniciais/app.js
+++ arquivos_iniciais/app.js
@@ -32,35 +32,58 @@ class Bd{
            localStorage.setItem(id, JSON.stringify(d))
            localStorage.setItem('id',id);
        }
+       recuperarTodosRegistros(){
+           let id = localStorage.getItem('id');
+           let despesas = Array();
+            for(let i = 1; i<=id;i++){
+                if(localStorage.getItem(i) === null){
+                    continue;
+                }
+                despesas.push(JSON.parse(localStorage.getItem(i)));
+            }
+            return despesas;
+       }
     }
-function cadastrarDespesas(){
-    let ano         = document.getElementById('ano');
-    let mes         = document.getElementById('mes');
-    let dia         = document.getElementById('dia');
-    let tipo        = document.getElementById('tipo');
-    let descricao   = document.getElementById('descricao');
-    let valor       =     document.getElementById('valor');
-    
-    let despesa = new Despesa(ano.value, mes.value, dia.value,tipo.value, descricao.value,valor.value);
     let bd = new Bd();
-    
-
     function registroModal(modalHeader, modalTitle,modalBody,btnModal){
         document.getElementById('modal-header').classList.add(modalHeader);
         document.getElementById('modal-title').innerHTML = modalTitle;
         document.getElementById('modal-body').innerHTML =modalBody;
         document.getElementById('btn-modal').classList.add(btnModal); 
     }
+    function limparCampo(){
+        document.getElementById('ano').value = '';
+        document.getElementById('mes').value = '';
+        document.getElementById('dia').value = '';
+        document.getElementById('tipo').value = '';
+        document.getElementById('descricao').value = '';
+        document.getElementById('valor').value = '';
+    }
+    function cadastrarDespesas(){
+    let ano         = document.getElementById('ano');
+    let mes         = document.getElementById('mes');
+    let dia         = document.getElementById('dia');
+    let tipo        = document.getElementById('tipo');
+    let descricao   = document.getElementById('descricao');
+    let valor       = document.getElementById('valor');
+    
+    let despesa = new Despesa(ano.value, mes.value, dia.value,tipo.value, descricao.value,valor.value);
+
 
     if(despesa.validarDados()){
         registroModal('text-success','Registro inserido com sucesso!','Despesa foi cadastrado com sucesso','btn-success');
         document.getElementById('btn-modal').innerHTML = 'Voltar'
         $('#modalRegistro').modal('show')       
         bd.gravar(despesa);
+        limparCampo();
     }else{
         registroModal('text-danger','Erro na gravação!','Existem campos obrigatórios que não foram preenchidos','btn-danger');
         document.getElementById('btn-modal').innerHTML = 'Voltar e corrigir'
         $('#modalRegistro').modal('show');
     }
     
+   }
+
+   function carregarListaDespesas(){
+       console.log(bd.recuperarTodosRegistros());
    }
